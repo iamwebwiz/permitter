9114a9879fb3e64cd2fc25a155cacbfa440de7b6
Ezekiel <iamwebwiz@gmail.com>
2020-02-22 10:05:22 +0100

    Update seeders

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use App\UserDetail;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -9,23 +11,64 @@ class UsersTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        //
+        $this->createApplicant()
+            ->createReviewer()
+            ->createProcessor();
+
+        $this->command->info('Users have been added to the database');
     }
 
-    private function createApplicant()
+    /**
+     * Generate an applicant
+     *
+     * @return $this
+     */
+    private function createApplicant(): self
     {
-        //
+        $applicant = factory(User::class)->create([
+            'name' => 'Ezekiel',
+            'last_name' => 'Oladejo',
+            'email' => 'iamwebwiz@gmail.com',
+        ]);
+
+        factory(UserDetail::class)->create([
+            'user_id' => $applicant->id,
+            'gender' => UserDetail::MALE_GENDER,
+            'occupation' => 'Software Developer',
+        ]);
+
+        $applicant->assignRole(User::APPLICANT_ROLE);
+
+        return $this;
     }
 
-    private function createReviewer()
+    /**
+     * Generate a reviewer
+     *
+     * @return $this
+     */
+    private function createReviewer(): self
     {
-        //
+        $reviewer = factory(UserDetail::class)->create()->user;
+
+        $reviewer->assignRole(User::REVIEWER_ROLE);
+
+        return $this;
     }
 
-    private function createProcessor()
+    /**
+     * Generate a processor
+     *
+     * @return $this
+     */
+    private function createProcessor(): self
     {
-        //
+        $processor = factory(UserDetail::class)->create()->user;
+
+        $processor->assignRole(User::PROCESSOR_ROLE);
+
+        return $this;
     }
 }

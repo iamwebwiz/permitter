f2db6622d8f2baf2dd03433699bcf14f4e647385
Ezekiel <iamwebwiz@gmail.com>
2020-02-22 11:22:43 +0100

    Add ApplicationController

---
+++ app/Http/Controllers/Applicant/ApplicationController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Applicant;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ApplicationController extends Controller
+{
+    //
+}

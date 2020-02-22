fc1bc26eb8c5bded0152065f46c65f314b4da863
Ezekiel <iamwebwiz@gmail.com>
2020-02-22 16:03:03 +0100

    Update reviewer route file

--- routes/reviewer.php
+++ routes/reviewer.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\Reviewer\ApplicationController;
 use App\Http\Controllers\Reviewer\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::prefix('applications')->group(static function () {
+    Route::get('{application}', [ApplicationController::class, 'show'])->name('applications.show');
+});

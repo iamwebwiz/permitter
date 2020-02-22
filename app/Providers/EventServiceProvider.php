7146b3115895d584f48c52fd6807f65499ae462c
Ezekiel <iamwebwiz@gmail.com>
2020-02-22 11:22:30 +0100

    Update Event Service Provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,11 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use App\Events\ApplicationSubmitted;
+use App\Listeners\Applications\SendNotificationToReviewer;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +19,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        ApplicationSubmitted::class => [
+            SendNotificationToReviewer::class,
+        ],
     ];
 
     /**
@@ -25,10 +29,8 @@ class EventServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         parent::boot();
-
-        //
     }
 }
